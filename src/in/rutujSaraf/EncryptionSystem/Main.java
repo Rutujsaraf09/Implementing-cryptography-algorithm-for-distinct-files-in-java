fba7cfb93527ca2290c1e2eb11bced6f95bd0b62
Rutujsaraf09 <rutujsaraf99@gmail.com>
2023-06-21 00:08:25 +0530

    first commit

---
+++ src/in/rutujSaraf/EncryptionSystem/Main.java
@@ -0,0 +1,22 @@
+package in.rutujSaraf.EncryptionSystem;
+
+//import java.awt.*;
+//import java.awt.image.BufferedImage;
+//import java.io.*;
+//import javax.imageio.ImageIO;
+//import javax.swing.JFrame;
+//import javax.swing.*;
+
+
+
+public class Main {
+
+  public Main() {
+
+	new Cryptography();
+	
+}
+public static void main(String args[]) throws Exception {
+  	new Main();
+  }
+}
